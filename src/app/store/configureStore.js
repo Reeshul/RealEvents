b9f64f3d1bb99f0c57e4cc89620dea2552aa35f9
carrot <reeshul@hotmail.com>
2019-12-13 14:00:13 +0000

    Authentication and Social Login

--- src/app/store/configureStore.js
+++ src/app/store/configureStore.js
@@ -4,12 +4,13 @@ import { reduxFirestore, getFirestore } from 'redux-firestore';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import rootReducer from '../reducers/rootReducer';
 import thunk from 'redux-thunk';
-import firebase from '../config/firebase'
+import firebase from '../config/firebase';
 
 const rrfConfig = {
   userProfile: 'users',
   attachAuthIsReady: true,
-  useFirestoreForProfile: true
+  useFirestoreForProfile: true,
+  updateProfileOnLogin: false
 };
 
 export const configureStore = () => {
